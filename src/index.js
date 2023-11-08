2b5bc8a149ab36a1f4b639856c350a69a517c108
Akari <87920616+akrmyuc@users.noreply.github.com>
2023-11-08 14:19:03 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>How are you?</p>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
